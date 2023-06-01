7ccecfd0e9a47cdb0133604eb85d13ceea1bdc58
tmawuli <timothykwame926@gmail.com>
2023-06-01 14:50:08 +0000

    pres and app page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 //import { useState } from 'react'
-import Home from './pages/Home/Home'
-import './App.css'
-import { Route } from 'react-router'
-import { BrowserRouter, Routes } from 'react-router-dom'
-
+import Home from './pages/Home/Home';
+import Prescription_form from './pages/Prescription_form/Prescription_form';
+import Appointment from './pages/Appointment/Appointment';
+import { Route } from 'react-router';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import './App.css';
 function App() {
 
   return (
@@ -11,6 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="appointment" element={<Appointment />} />
+         <Route path="prescription_form" element={<Prescription_form />} />
          </Routes>
   {/* Additional routes */}
        </BrowserRouter>
